9726d14ec8030379fb974e5f4085796a7db08764
optimiste-hazel <optimiste.hazel@gmail.com>
2024-06-11 15:45:51 +0000

    ajout de GETTER et SETTER au model AUTHOR

--- src/main/java/dev/bkjulien/data_jpa/models/AhthorModel.java
+++ src/main/java/dev/bkjulien/data_jpa/models/AhthorModel.java
@@ -16,4 +16,19 @@ public class AhthorModel implements Serializable {
     @Column(nullable = false, unique = true)
     private String name;
 
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
